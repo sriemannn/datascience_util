d132c0cd9ee086455e9cd01e57aca5fbe67694da
sriemannn <steffenriemann@web.de>
2025-01-09 12:42:14 +0100

    added one hot encoder and ordinal encoder, if I don't want to add scikit-learn for this

--- helper.py
+++ helper.py
@@ -158,3 +158,50 @@ def plot_missing_values(train, test):
     _ = ax[1].set_title("Missing values in test set")
 
     plt.show()
+
+def ordinal_encoder(col: pd.Series, mapping: dict | None = None) -> pd.Series:
+    """
+    Encode categorical variable as ordinal.
+
+    Parameters:
+    -----------
+    col : pd.Series
+        The categorical variable to be encoded.
+
+    Returns:
+    --------
+    pd.Series
+        The encoded categorical variable.
+    """
+
+    n_unique = set(col.to_list())
+    if mapping is None:
+        mapping = {k: i for i, k in enumerate(n_unique, 0)}
+    return col.map(mapping)
+
+
+def one_hot_encoder(
+    df: pd.DataFrame, cols: str | list, drop_first: bool = True
+) -> pd.DataFrame:
+    """
+    Encode categorical variable as one-hot.
+
+    Parameters:
+    -----------
+    df : pd.DataFrame
+        The dataframe containing the categorical variable.
+    col : pd.Series
+        The categorical variable to be encoded.
+    drop_first : bool, default=True
+        Whether to drop the first column of the encoded variable.
+
+
+    Returns:
+    --------
+    pd.DataFrame
+        The encoded categorical variable.
+    """
+    if isinstance(cols, str):
+        cols = [cols]
+
+    return pd.get_dummies(df, columns=cols, drop_first=drop_first, dtype=int)
